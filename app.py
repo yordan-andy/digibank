8e35c99fabd4ce9e03f0ca490fa25d475f0086ef
yordan-andy <andy.yordan@gmail.com>
2021-04-16 20:00:26 +0700

    update cara logout

--- app.py
+++ app.py
@@ -3,18 +3,11 @@ from flask import Flask, request
 from flask_restful import Resource, Api
 from flask_cors import CORS
 
-import time
-import urllib.request
-import urllib.parse
-import urllib.error
-import getpass
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.support.ui import Select
-from bs4 import BeautifulSoup
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.utils import ChromeType
 
@@ -36,7 +29,7 @@ CORS(app)
 identitas = {}
 
 
-class BankApp(Resource):
+class klikDBS(Resource):
     __url = 'https://internet-banking.dbs.com.sg/'
 
     def get(self):
@@ -51,7 +44,6 @@ class BankApp(Resource):
         self.__password = password
         response = self.login()
         return response
-        # return {"message": "Login sukses"}
 
     def login(self):
         opts = webdriver.ChromeOptions()
@@ -59,61 +51,44 @@ class BankApp(Resource):
         self.__driver = webdriver.Chrome(
             ChromeDriverManager(chrome_type=ChromeType.GOOGLE).install(), options=opts)
         self.__driver.wait = WebDriverWait(self.__driver, 3)
+        self.__driver.get(self.__url)
 
-        try:
-            self.__driver.get(self.__url)
-            username = self.__driver.wait.until(
-                EC.presence_of_element_located((By.ID, "UID")))
-            password = self.__driver.wait.until(
-                EC.presence_of_element_located((By.ID, "PIN")))
-            login_bca = self.__driver.wait.until(
-                EC.element_to_be_clickable((By.XPATH, "//*[text()='Login']")))
-            username.send_keys(self.__username)
-            password.send_keys(self.__password)
-            login_bca.send_keys(webdriver.common.keys.Keys.SPACE)
+        username = self.__driver.wait.until(
+            EC.presence_of_element_located((By.ID, "UID")))
+        password = self.__driver.wait.until(
+            EC.presence_of_element_located((By.ID, "PIN")))
+        login = self.__driver.wait.until(
+            EC.element_to_be_clickable((By.XPATH, "//*[text()='Login']")))
 
-            try:
-                self.__driver.switch_to.frame(
-                    self.__driver.find_element_by_name("user_area"))
-                self.__driver.switch_to.default_content()
-                return self.cekSaldo()
-            except:
-                alert = self.__driver.switch_to.alert()
-                # return alert.text
-                return {"message": "Login gagal 1"}
-                alert.accept()
+        username.send_keys(self.__username)
+        password.send_keys(self.__password)
+        login.send_keys(webdriver.common.keys.Keys.SPACE)
 
-        except:
-            return {"message": "Login gagal 2"}
+        self.__driver.switch_to.frame(
+            self.__driver.find_element_by_name("user_area"))
+        self.__driver.switch_to.frame(
+            self.__driver.find_element_by_name("iframe1"))
 
-    def cekSaldo(self):
-        try:
-            self.__driver.switch_to.frame(
-                self.__driver.find_element_by_name("user_area"))
-            self.__driver.switch_to.frame(
-                self.__driver.find_element_by_name("iframe1"))
-
-            saldo = self.__driver.find_element_by_xpath(
-                "//*[contains(text(), \"S$\")]").text
-            self.__driver.switch_to.default_content()
-            self.logout()
-            return {"message": "Saldo DBS saat ini adalah %s" % saldo}
-        except TimeoutException:
-            return {"message": "Session timeout. please login again"}
+        saldo = self.__driver.find_element_by_xpath(
+            "//*[contains(text(), \"S$\")]").text
+        self.__driver.switch_to.default_content()
+        self.logout()
+        return {"message": "Saldo DBS saat ini adalah %s" % saldo}
 
     def logout(self):
         try:
             self.__driver.switch_to.frame(
                 self.__driver.find_element_by_name("user_area"))
             logout = self.__driver.wait.until(EC.presence_of_element_located(
-                (By.XPATH, "//a[contains(@href, \"javascript:goToState\")]")))
+                (By.XPATH, "//a[contains(@href, \"LogOutUser\")]")))
             logout.click()
             print("Anda berhasil logout")
         except TimeoutException:
             print("Session timeout. please login again")
 
-    # setup resource
-api.add_resource(BankApp, "/api", methods=["GET", "POST"])
+
+# setup resource
+api.add_resource(klikDBS, "/api", methods=["GET", "POST"])
 
 if __name__ == "__main__":
     app.run(debug=True, port=5005)
